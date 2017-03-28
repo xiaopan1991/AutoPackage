66e359d5090bf7c7de8cf297f9feebb4ec768be0
xiaopan1991 <xiaopan126@126.com>
2017-03-28 21:53:04 +0800

    1111

---
+++ Assets/Editor/CMDTool.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+
+public class CMDTool  {
+
+	[MenuItem("Tools/Build Android")]
+	public static void BuildAndroid()
+	{
+		string app_name = "abc.apk";
+		string applicationPath = Application.dataPath.Replace("/Assets", "/apk");
+
+		PlayerSettings.companyName = "xiaopan";
+		PlayerSettings.productName = "AutoPublicPackage";
+		PlayerSettings.bundleIdentifier = "com.xiaopan.AutoPublicPackage";
+		PlayerSettings.bundleVersion = "1.0";
+
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.Android);
+
+		BuildPipeline.BuildPlayer(new string[]{"Assets/Scenes/Main.unity"}, 
+			applicationPath+"/"+app_name, BuildTarget.Android, BuildOptions.None);
+	}
+
+}
